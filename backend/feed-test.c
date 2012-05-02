ebc3b2065c495d6f16b7fb1f4d2f272546dbed95
Jos Manuel Ciges Regueiro <jmanuel@ciges.net>
2012-05-02 18:46:42 +0200

    Added search by type (movies, shows or episodes).
    Also API key can now be in a text file and general source code
    cleaning.
    
    This patch makes available a new version of feed-test with two
    fonctions added:
    
    * The user has now the possibility of search the term by types of
      content (movies, shows or episodes). This function was available in
      the GtFeed class used but not available to the user via options
    
    * now the API key is searched in ~/.config/.trakttv_apikey text file
      if not given as parameter. It was really unconfortable having the
      need of writing the API key each time the tool is used
    
    * A better argument parsing. There is no query by default, lack of
      query option gives an error.
    
    * The source code is a lot clearer: 'test1' function has been renamed
      to query_trakttv
    
    Signed-off-by: Víctor Manuel Jáquez Leal <vjaquez@igalia.com>

--- backend/feed-test.c
+++ backend/feed-test.c
@@ -2,9 +2,18 @@
 
 #include "gt-feed.h"
 
+#define APIKEY_FILE ".trakttv_apikey"
+
 static char *apikey;
-static char *query = "batman";
+static char *query;
+
 static GMainLoop *loop;
+
+static gboolean search_movies = FALSE;
+static gboolean search_shows = FALSE;
+static gboolean search_episodes = FALSE;
+
+GtFeedSearchType searchtype;
 
 static GOptionEntry entries[] = {
         {
@@ -15,6 +24,27 @@ static GOptionEntry entries[] = {
 		.description = "trakt.tv API key",
 	},
         {
+		.long_name = "movies",
+		.short_name = 'm',
+		.arg = G_OPTION_ARG_NONE,
+		.arg_data = &search_movies,
+		.description = "Limit search only to movies (default value)",
+	},
+        {
+		.long_name = "shows",
+		.short_name = 's',
+		.arg = G_OPTION_ARG_NONE,
+		.arg_data = &search_shows,
+		.description = "Limit search only to shows",
+	},
+	{
+		.long_name = "episodes",
+		.short_name = 'e',
+		.arg = G_OPTION_ARG_NONE,
+		.arg_data = &search_episodes,
+		.description = "Limit search only to episodes",
+	},
+	{
 		.long_name = "query",
 		.short_name = 'q',
 		.arg = G_OPTION_ARG_STRING,
@@ -26,7 +56,7 @@ static GOptionEntry entries[] = {
 	}
 };
 
-void
+static void
 cb(GObject *source, GAsyncResult *res, void *data)
 {
 	GError *err = NULL;
@@ -51,16 +81,49 @@ bail:
 	return;
 }
 
-gboolean
-test1(void *data)
+static gboolean
+query_trakttv(void *data)
 {
 	GtFeed *feed = g_object_new(GT_TYPE_FEED, "api-key", apikey, NULL);
-	if (!gt_feed_search(feed, GT_FEED_SEARCH_MOVIES, query, cb, NULL)) {
+	if (!gt_feed_search(feed, searchtype, query, cb, NULL)) {
 		g_main_loop_quit(loop);
 	}
         g_object_unref(feed);
 
 	return FALSE;
+}
+
+static gboolean
+read_api_key()
+{
+	gchar *file = NULL;
+	gchar *contents = NULL;
+	GError *error = NULL;
+	gboolean ret = FALSE;
+
+	file = g_build_path(g_get_user_config_dir(), APIKEY_FILE, NULL);
+	if (g_file_test(file, G_FILE_TEST_EXISTS))  {
+		g_print("No API key provided nor %s file exists!\n", file);
+		goto bail;
+	}
+
+	if (!g_file_get_contents(file, &contents, NULL, &error))  {
+		g_print("Reading the API key from file %s failed: %s\n",
+			file, error->message);
+		g_error_free(error);
+		goto bail;
+	}
+
+	apikey = contents;
+	g_strstrip(apikey);
+
+	ret = TRUE;
+
+bail:
+	g_free(file);
+	g_free(contents);
+
+	return ret;
 }
 
 int
@@ -79,16 +142,36 @@ main (int argc, char **argv)
 			g_print("option parsing failed: %s\n", error->message);
 			return -1;
 		}
+
+		if (!query) {
+			g_critical("No query provided!\n\n");
+
+			g_print(g_option_context_get_help (context, TRUE, NULL));
+			return -1;
+		}
+
+		if (!apikey && !read_api_key()) {
+			g_print(g_option_context_get_help(context, TRUE, NULL));
+			return -1;
+		}
+
+		g_option_context_free(context);
 	}
 
-	if (!apikey) {
-		g_warning("No API key provided.");
-		return -1;
+	searchtype = GT_FEED_SEARCH_MOVIES;
+	if (search_movies)  {
+		searchtype = GT_FEED_SEARCH_MOVIES;
+	}
+	else if (search_shows)  {
+		searchtype = GT_FEED_SEARCH_SHOWS;
+	}
+	else if (search_episodes) {
+		searchtype = GT_FEED_SEARCH_EPISODES;
 	}
 
 	loop = g_main_loop_new(NULL, TRUE);
-	g_idle_add(test1, NULL);
+	g_idle_add(query_trakttv, NULL);
 	g_main_loop_run(loop);
 
-        return 0;
+	return 0;
 }
