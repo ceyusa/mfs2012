aaca2c6753150e76fe4628d41e7306eb626ac89f
Joaquim Rocha <jrocha@igalia.com>
2012-05-10 12:25:40 +0200

    Add Traktor's base window

---
+++ ui/gtk/traktor/traktorwindow.py
@@ -0,0 +1,52 @@
+from gi.repository import Gtk
+
+MENU_UI = '''
+<ui>
+    <menubar name="MenuBar">
+        <menu action="TraktorMenu">
+            <menuitem action="About" />
+        </menu>
+    </menubar>
+</ui>
+'''
+
+class TraktorWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self)
+        self.set_title('Traktor')
+        self.set_size_request(500, 300)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(box)
+
+        self.ui_manager = self._setup_ui_manager()
+        menu_bar = self.ui_manager.get_widget('/MenuBar')
+        box.pack_start(menu_bar, False, False, 0)
+
+        self.connect('delete-event', self._quit)
+
+    def _setup_ui_manager(self):
+        ui_manager = Gtk.UIManager()
+        ui_manager.add_ui_from_string(MENU_UI)
+        accel_group = ui_manager.get_accel_group()
+        self.add_accel_group(accel_group)
+        action_group = Gtk.ActionGroup('Actions')
+        action_group.add_actions([
+                ('TraktorMenu', None, '_Traktor', None, None, None),
+                ('About', Gtk.STOCK_ABOUT,
+                 '_About', None, 'About this application',
+                 self._on_about_action),
+                ])
+        ui_manager.insert_action_group(action_group)
+        return ui_manager
+
+    def _on_about_action(self, action):
+        print 'Preferences action activated [DELETE THIS WHEN THE ABOUT IS IMPLEMENTED]'
+
+    def _quit(self, window, event):
+        Gtk.main_quit()
+
+    def run(self):
+        self.show_all()
+        Gtk.main()
